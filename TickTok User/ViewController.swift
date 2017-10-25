1e5989661726c09e44e3f6e8f6095f7a6da17c88
Excellent Webworld <developer.eww@gmai.com>
2017-10-25 14:46:01 +0530

    Initial Commit

---
+++ TickTok User/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TickTok User
+//
+//  Created by Excellent Webworld on 25/10/17.
+//  Copyright © 2017 Excellent Webworld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
